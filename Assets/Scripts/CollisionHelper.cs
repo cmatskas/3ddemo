60b5e4ef852cde84e0ac68002d67765e6eda7b09
Christos Matskas <chmatsk@microsoft.com>
2019-09-21 08:59:52 -0700

    first working solution

---
+++ Assets/Scripts/CollisionHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.Networking.Match;
+
+public class CollisionHelper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.name == "Vehicle")
+        {
+           Explode();
+        }
+    }
+
+    void Explode()
+    {
+        var exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        Destroy(gameObject, exp.duration);
+        Destroy(exp);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 20.0f;
+    private float turnSpeed =45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
